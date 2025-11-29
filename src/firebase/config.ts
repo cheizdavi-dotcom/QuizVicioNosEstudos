c6cfe5bd7da71b554253fb5b6c2d26e6f0b48d7a
Davi Cheiz <davicheizz9@gmail.com>
2025-11-29 15:46:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5865898501-1380e",
+  "appId": "1:628337078692:web:d7d24cfc1f30446d88d131",
+  "apiKey": "AIzaSyCVS6-uX-VnTXyu-_LqN1IADkNbVWdoHCA",
+  "authDomain": "studio-5865898501-1380e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "628337078692"
+};
